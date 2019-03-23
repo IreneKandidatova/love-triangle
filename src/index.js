8b404bcad53827a731953b71723cdf425ea8f60e
IreneKandidatova <47784868+IreneKandidatova@users.noreply.github.com>
2019-03-23 21:11:27 +0300

    add implementation

--- src/index.js
+++ src/index.js
@@ -4,4 +4,22 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  var TrCount = 0;
+  preferences.unshift(0);
+  var i = 1;
+    while (i < preferences.length - 2) {
+    var first = i;
+    var second = preferences[i];
+    var third = preferences[first];
+    if (first !=second){
+      var third = preferences[second];
+      if (second != third){
+        var fourth = preferences[third];
+                if (fourth == first) { 
+                  ++TrCount;
+                  preferences[i] = 0;
+                  }}
+    i++;            
+     }
+      return TrCount
 };
